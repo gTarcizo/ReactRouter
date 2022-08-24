9f270839b1fe5fcc0371ee71f1e662a2a342754e
gTarcizo <90656673+gTarcizo@users.noreply.github.com>
2022-08-24 18:05:58 -0300

    Rota aninhada

---
+++ tutorial-react-route-dom/src/components/Post/index.tsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+export const Post = () => {
+    return (
+        <div>
+        <br />
+        <h2>Post</h2>
+        <br />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, ad? Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, omnis?</p>
+    </div>
+    )
+}
